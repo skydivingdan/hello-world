857fbe3365e1c94d369ed69963e78cde6963f78d
root <root@localhost.localdomain>
2016-07-29 16:35:39 -0500

    From this thing

---
+++ lpthw/ex10.py
@@ -0,0 +1,24 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm spliting\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = '''
+I'll do a list:
+\t* Cat food
+\t* Fishies 
+\t* Catnip\n\t* Grass
+'''
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+#x = 42000
+#print x
+#while (x > 0):
+#  for i in ["/","-","|","\\","|"]:
+#    print "%s\r" %i,
+#    print x,
+#  x = x - 1
+  
---
+++ lpthw/ex11_sup.py
@@ -0,0 +1,11 @@
+print "Answer three questions to cross the bridge" 
+print "\n\tWhat is your name?"
+name = raw_input()
+print "\tWhat is your quest?",
+quest = raw_input()
+print "\tWhat is the average airspeed velocity of an Unlaiden swallow?"
+swallow = raw_input() 
+if swallow == "African or European?": 
+  print "AHHHHHHHHHHHHHH" 
+else:
+  print "pass"
