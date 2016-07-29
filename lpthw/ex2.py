3804e90a0ed68d951ebfa4ab46348729c9835d42
Dan Patterson <dan@bluesmokellc.net>
2016-07-28 21:29:36 -0500

    commit test

---
+++ lpthw/ex2.py
@@ -0,0 +1,8 @@
+# A comment, this is so you can read your program later
+# anything after the # is ignored by the python like snakey thing.... and the beedy eyes
+
+print "I could have coded like this" # this won't print
+# print "This won't run"
+print "#print('this won't run')"
+
+print "This will run."
